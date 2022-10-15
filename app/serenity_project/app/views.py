3634634ecf6935f90cdd93c507f003d49766bc1c
Najeer Ahmed <80684217+najeerahmed@users.noreply.github.com>
2022-10-15 11:43:10 -0400

    attempt at index page with search bar
    running into an issue with returning the correct url

--- app/serenity_project/app/views.py
+++ app/serenity_project/app/views.py
@@ -4,7 +4,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import ScoreTable
 from .serializers import ScoreTableSerializer
+from django.shortcuts import render
 
 class ScoreTableViewSet(viewsets.ModelViewSet):
     queryset = ScoreTable.objects.all()
     serializer_class = ScoreTableSerializer
+
+def index(request):
+    latest_zipcode_list = ScoreTable.objects.order_by('zipcode')[:5]
+    context = {
+        'latest_zipcode_list':latest_zipcode_list
+    }
+    return render(request, 'app/index.html', context)
+
+def search(request):
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        post = ScoreTable.objects.all().filter(zipcode=search)
+        return render(request, 'app/index/search.html', {'post' : post})
