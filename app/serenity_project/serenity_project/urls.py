502ca3668d456cab66d6292d156b746c88a20425
Aigerim Madakimova <aigerim.madakimova@aiesec.net>
2022-10-15 20:59:18 -0400

    home page

--- app/serenity_project/serenity_project/urls.py
+++ app/serenity_project/serenity_project/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   
-    path('', include('frontend.urls'))
+    path('', include('app.urls')),
+    path('home/', include('frontend.urls'))
 ]
