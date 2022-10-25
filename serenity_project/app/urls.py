235c99ce527117f77cb304c0551af72ccaeea1f1
Rtlyc <yuchenliu0513@gmail.com>
2022-10-25 11:25:27 -0400

    change django router link

--- serenity_project/app/urls.py
+++ serenity_project/app/urls.py
@@ -7,14 +7,16 @@ from django.conf.urls.static import static
 # from django.conf.urls import patterns,url
 from . import views
 
-router = DefaultRouter()
-router.register("api", ScoreTableViewSet)
+## api router
+api_router = DefaultRouter()
+api_router.register("table", ScoreTableViewSet)
 
 urlpatterns = [
-    path("index/", views.index, name="index"),
-    path("", include(router.urls)),
-    path("index/search", views.search, name="search"),
+    path("", views.index),
+    path("api/", include(api_router.urls)),
+    path("search", views.search, name="search"),
     path("register", views.register_request, name="register"),
     path("login", views.login_request, name="login"),
     path("map", views.map_view, name="map"),
 ]
+
