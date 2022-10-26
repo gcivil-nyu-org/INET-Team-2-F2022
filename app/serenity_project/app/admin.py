d925a630141c6b8bf6cce6d5e253a0076539cdd8
Rtlyc <yuchenliu0513@gmail.com>
2022-10-26 12:59:37 -0400

    clear develop

--- app/serenity_project/app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ScoreTable
-# Register your models here.
-
-admin.site.register(ScoreTable)
--- app/serenity_project/app/serializers.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import field
-from rest_framework import serializers
-from .models import ScoreTable
-
-class ScoreTableSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = ScoreTable
-        fields = '__all__'
