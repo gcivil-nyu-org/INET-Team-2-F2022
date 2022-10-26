36d644cffcdeb0ec9ceb1767ce5f9aaefaa5c084
Abhi Dasari <abhidasari@Sris-MBP.lan>
2022-10-26 17:25:36 -0400

    adding basic test and coveralls in travis config

--- serenity_project/app/tests.py
+++ serenity_project/app/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from .models import ScoreTable
 
+
 class AppViewTests(TestCase):
     def test_home_endpoint_returns_welcome_page(self):
         response = self.client.get(path="/")
