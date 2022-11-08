3247581b00476c0ffe7e3b279fa2f83db15878b1
Abhi Dasari <abhidasari@Sris-MBP.lan>
2022-11-07 19:35:23 -0500

    renaming -discuss- in comment model to -comment-

--- serenity_project/app/migrations/0014_rename_discuss_comment_comment.py
+++ serenity_project/app/migrations/0014_rename_discuss_comment_comment.py
@@ -6,13 +6,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0013_name_email_comment'),
+        ("app", "0013_name_email_comment"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='comment',
-            old_name='discuss',
-            new_name='comment',
+            model_name="comment",
+            old_name="discuss",
+            new_name="comment",
         ),
     ]
