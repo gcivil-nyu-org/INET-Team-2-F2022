f36e7637f8cf9d519a19ebf0c952af7791eba385
Najeer Ahmed <80684217+najeerahmed@users.noreply.github.com>
2022-11-09 14:52:20 -0500

    Update forms.py

--- serenity_project/app/forms.py
+++ serenity_project/app/forms.py
@@ -14,6 +14,7 @@ class NewUserForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
         if commit:
             user.save()
         return user
