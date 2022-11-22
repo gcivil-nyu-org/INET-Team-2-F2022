b5135b99c1db98059d9b60e93ced7e86aec680e4
Najeer Ahmed <80684217+najeerahmed@users.noreply.github.com>
2022-11-22 12:25:59 -0500

    Update settings.py

--- serenity_project/serenity_project/settings.py
+++ serenity_project/serenity_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic"
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
